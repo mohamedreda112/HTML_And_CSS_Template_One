ba6b37f749885bcdc6d9a00a08ed4b529dca0888
mohamedreda112 <96078085+mohamedreda112@users.noreply.github.com>
2024-04-17 15:06:38 +0200

    HTML_And_CSS_Template_One

---
+++ main.js
@@ -0,0 +1,15 @@
+let icon = document.getElementById("icon")
+let ul = document.getElementById("ul")
+// icon.onclick = function(){
+//     ul.style.display = "block"
+// }
+// body.onclick = function(){
+//     ul.style.display = "none"
+// }
+
+icon.addEventListener("click", function(){
+    ul.style.display = "block";
+})
+
+
+console.log(icon)
